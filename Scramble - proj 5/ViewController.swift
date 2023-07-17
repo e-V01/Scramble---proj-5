cea7354be153543367630bceb77d7631af4dc518
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-07-17 16:43:00 +0300

    1st commit

--- Scramble - proj 5/ViewController.swift
+++ Scramble - proj 5/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
     }
+    
 
-
+    func startGame() {
+        title = allWords.randomElement() // set view controller inside it in random
+        usedWords.removeAll(keepingCapacity: true) // removes all values from used words
+        tableView.reloadData() // reload all rows and sections from scratch
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+        
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row] // all words user found so far and show they found these words
+        return cell
+    }
 }
 
